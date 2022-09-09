d150a45a626dedc789af9c9e6a851fcf052ac5ae
Melinsa-lane <113193687+Melinsa-lane@users.noreply.github.com>
2022-09-09 21:52:18 +0700

    Add files via upload

---
+++ Lab_1   test.cpp
@@ -0,0 +1,24 @@
+﻿
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	unsigned long long d = ULLONG_MAX, CalcLLInt, var; //Calc - расчетное, var просто для теста
+	CalcLLInt = pow(2, 8 * sizeof(d)) - 1; // По данным Microsoft: pow не распознает
+										   // целочисленные значения с плавающей запятой больше 2^64.
+
+	cout << "Константа максимального значения для unsigned long long: " << d
+		<< "\nРасчетное значение: " << CalcLLInt << endl;
+
+	var = pow(2, 63);
+	cout << endl << var << endl;
+
+	var *= 2; //2^64
+	cout << var << endl;
+
+	var -= 1;
+	cout << var << endl;
+}
